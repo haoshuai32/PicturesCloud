d83a7307dc38d5ba86bcd7455e5adfb620f915f0
haoshuai <haoshuai9192@gmail.com>
2022-05-23 21:20:59 +0800

    实现itemcell

---
+++ PicturesCloud/PictureCell.swift
@@ -0,0 +1,17 @@
+//
+//  PictureCell.swift
+//  PicturesCloud
+//
+//  Created by haoshuai on 2022/5/23.
+//
+
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
